631fe47eddc40255cd0ef33b1f34f806eded8d0f
Matheus Faé <matheusfae@gmail.com>
2026-07-04 20:29:16 -0300

    Melhorias visuais

--- lib/widgets/snackbar/snackbar_service.dart
+++ lib/widgets/snackbar/snackbar_service.dart
@@ -8,8 +8,15 @@ class SnackbarService {
   static void snackErro(String message){
     scaffoldMessengerKey.currentState?.showSnackBar(
       SnackBar(
-        content: Text(message, style: const TextStyle(color: Color.fromARGB(255, 250, 88, 77))),
-        persist: true,
+        content: Text(
+          message, 
+          style: const TextStyle(color: Color.fromARGB(255, 92, 6, 0))
+          ),
+        backgroundColor: Color.fromARGB(255, 255, 192, 192),  
+        // persist: true,
+        showCloseIcon: true,
+        closeIconColor: Color.fromARGB(255, 92, 6, 0),
+        duration: const Duration(seconds: 3),
       ),
     );
   }
@@ -17,8 +24,13 @@ class SnackbarService {
   static void snackSucesso(String message){
     scaffoldMessengerKey.currentState?.showSnackBar(
       SnackBar(
-        content: Text(message, style: const TextStyle(color: Color.fromARGB(255, 0, 255, 0))),
-        persist: true,
+        content: Text(
+          message, 
+          style: const TextStyle(color: Color.fromARGB(255, 113, 250, 113))
+          ),
+        // persist: true,
+        showCloseIcon: true,
+        duration: const Duration(seconds: 20),
       ),
     );
   }
@@ -26,8 +38,15 @@ class SnackbarService {
   static void snackInfo(String message){
     scaffoldMessengerKey.currentState?.showSnackBar(
       SnackBar(
-        content: Text(message, style: const TextStyle(color: Color.fromARGB(255, 51, 51, 249))),
-        persist: true,
+        content: Text(
+          message, 
+          style: const TextStyle(color: Color.fromARGB(255, 127, 127, 253))
+          ),
+        backgroundColor: Color.fromARGB(255, 0, 109, 225),  
+        // persist: true,
+        showCloseIcon: true,
+        closeIconColor: Color.fromARGB(255, 127, 127, 253),
+        duration: const Duration(seconds: 20),
       ),
     );
   }
@@ -35,8 +54,15 @@ class SnackbarService {
   static void snackAviso(String message){
     scaffoldMessengerKey.currentState?.showSnackBar(
       SnackBar(
-        content: Text(message, style: const TextStyle(color: Color.fromARGB(255, 255, 255, 0))),
-        persist: true,
+        content: Text(
+          message, 
+          style: const TextStyle(color: Color.fromARGB(255, 250, 250, 184))
+          ),
+        backgroundColor: Color.fromARGB(255, 233, 233, 2),  
+        // persist: true,
+        showCloseIcon: true,
+        closeIconColor: Color.fromARGB(255, 250, 250, 184),
+        duration: const Duration(seconds: 20),
       ),
     );
   }
