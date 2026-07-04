ec4378d560e5df7ddc4baf823685fa0358c68da4
Matheus Faé <matheusfae@gmail.com>
2026-07-04 20:30:21 -0300

    Tela base da aplicação, que confere se há usuário logado

---
+++ lib/screens/auth/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:appcompanion/screens/auth/login_screen.dart';
+import 'package:appcompanion/screens/usuario/lista_usuarios.dart';
+import 'package:appcompanion/services/usuario_service.dart';
+import 'package:flutter/material.dart';
+import '../../services/auth_service.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLogin();
+  }
+
+  Future<void> _checkLogin() async {
+    final isLogged = await AuthService.isLoggedIn();
+
+    if (!mounted) return;
+
+    if (isLogged) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => ListaUsuariosScreen(usuarioService: UsuarioService(),)),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => LoginScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
