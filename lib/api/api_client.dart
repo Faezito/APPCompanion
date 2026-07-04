51e890d0ff1d9d98c52da325c936c66e5b1103c9
Matheus Faé <matheusfae@gmail.com>
2026-07-04 17:28:21 -0300

    Aumentado timeout

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -3,10 +3,10 @@ import 'package:dio/dio.dart';
 class ApiClient {
   static final Dio dio = Dio(
     BaseOptions(
-      // baseUrl: 'http://localhost:5130',
-      baseUrl: 'https://localhost:44339', // Replace with your API base URL
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
+      baseUrl: 'http://localhost:5130',
+      // baseUrl: 'https://localhost:44339',
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
       headers: {
         'Content-Type': 'application/json',
       },
