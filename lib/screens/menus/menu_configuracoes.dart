8435a3cb7fbc4a30331a99c2ab8dc4c9f5d917ba
Matheus Faé <matheusfae@gmail.com>
2026-07-06 20:33:38 -0300

    Criado menu de configurações do sistema

---
+++ lib/screens/menus/menu_configuracoes.dart
@@ -0,0 +1,51 @@
+import 'package:appcompanion/core/di/service_locator.dart';
+import 'package:appcompanion/screens/usuario/lista_usuarios.dart';
+import 'package:appcompanion/services/auth_service.dart';
+import 'package:appcompanion/widgets/base/appbar.dart';
+import 'package:appcompanion/widgets/botoes/menu_buttons.dart';
+import 'package:flutter/material.dart';
+
+class ConfigMenu extends StatefulWidget {
+  const ConfigMenu ({super.key});
+
+  @override
+  State<ConfigMenu> createState() => _ConfigMenuState();
+}
+
+class _ConfigMenuState extends State<ConfigMenu> {
+  final _auth = getIt<IAuthService>();
+  bool carregando = false;
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: BaseAppBar(titulo: "Configurações"),
+      body: Container(
+        padding: EdgeInsets.only(top: 40),
+        child: Align(alignment: Alignment.topCenter,
+          child: FractionallySizedBox(
+            widthFactor: 0.85,
+            child: Wrap(
+              spacing: 16,
+              runSpacing: 16,
+              alignment: WrapAlignment.spaceAround,
+              children: [
+                MenuButton(icon: Icons.people, title: "Usuários", onTap: () => _listaUsuarios())
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _listaUsuarios(){
+    Navigator.push(context, 
+    MaterialPageRoute(builder: (_) => ListaUsuariosScreen()));
+  }
+}
