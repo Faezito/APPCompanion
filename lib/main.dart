c2ffb6fa52b18cd817a3d7d73ac2316c66259d58
Matheus Faé <matheusfae@gmail.com>
2026-07-05 21:26:00 -0300

    Adicionado DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:appcompanion/api/api_client.dart';
+import 'package:appcompanion/core/di/service_locator.dart';
 import 'package:appcompanion/screens/auth/splash_screen.dart';
 import 'package:appcompanion/widgets/snackbar/snackbar_service.dart' as snackbar_service;
 import 'package:flutter/material.dart';
 
 void main() {
+  setupDependencies();
   ApiClient.init();
   runApp(const MyApp());
 }
