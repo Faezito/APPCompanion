0247068d6515190e1a4289f865acf1d10d70fa82
Matheus Faé <matheusfae@gmail.com>
2026-07-06 12:28:03 -0300

    Feita abstração da classe e migração inicial

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,4 +1,5 @@
 import 'package:appcompanion/services/acesso_service.dart';
+import 'package:appcompanion/services/auth_service.dart';
 import 'package:appcompanion/services/usuario_service.dart';
 import 'package:get_it/get_it.dart';
 
@@ -7,4 +8,5 @@ final getIt = GetIt.instance;
 void setupDependencies() {
   getIt.registerLazySingleton<IUsuarioService>(() => UsuarioService());
   getIt.registerLazySingleton<IAcessoService>(() => AcessoService());
+  getIt.registerLazySingleton<IAuthService>(() => AuthService());
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,61 +1,95 @@
+import 'dart:convert';
+
+import 'package:appcompanion/models/responses/usuario_response.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class AuthService {
+abstract class IAuthService {
+  Future<void> salvarSessao({required String token, required DateTime expiration, required int perfil, required UsuarioResponse usuario});
+  Future<DateTime?> getExpiration();
+  bool isLoggedIn();
+  Future<void> logout();
+  Future<void> restaurarSessao();
+  UsuarioResponse? get usuario;
+  bool get isAdmin;
+  String? get obterToken;
+}
+
+class AuthService implements IAuthService {
   static final FlutterSecureStorage _storage = FlutterSecureStorage();
 
   static const _tokenKey = 'token';
   static const _expirationKey = 'expiration';
-  static const _perfilKey = 'perfil';
+  static const _usuarioKey = 'usuario';
+  UsuarioResponse? _usuario;
+  String? _token;
+  DateTime? _expiration;
 
-  /// SALVAR LOGIN
-  static Future<void> salvarSessao({
+  @override
+  Future<void> salvarSessao({
     required String token,
     required DateTime expiration,
-    required int perfil
+    required int perfil,
+    required UsuarioResponse usuario
   }) async {
     await _storage.write(key: _tokenKey, value: token);
     await _storage.write(
       key: _expirationKey,
       value: expiration.toIso8601String(),
     );
-    await _storage.write(key: _perfilKey, value: perfil.toString());
+    await _storage.write(key: _usuarioKey, value: jsonEncode(usuario.toJson()));
+    _usuario = usuario;
+    _token = token;
+    _expiration = expiration;
   }
 
-  /// PEGAR TOKEN
-  static Future<String?> getToken() async {
-    return await _storage.read(key: _tokenKey);
+  @override
+  Future<DateTime?> getExpiration() async => _expiration;
+
+  @override
+  bool isLoggedIn() {
+    if (_token == null || _expiration == null) {
+      return false;
+    }
+
+    return DateTime.now().isBefore(_expiration!);
   }
 
-  /// Pegar Perfil
-  static Future<int?> obterPerfil() async {
-    final value = await _storage.read(key: _perfilKey);
-    if(value == null) return null;
-    return int.tryParse(value); 
-  }
-
-  /// PEGAR EXPIRAÇÃO
-  static Future<DateTime?> getExpiration() async {
-    final value = await _storage.read(key: _expirationKey);
-    if (value == null) return null;
-    return DateTime.tryParse(value);
-  }
-
-  /// VERIFICAR SE ESTÁ LOGADO
-  static Future<bool> isLoggedIn() async {
-    final token = await getToken();
-    final expiration = await getExpiration();
-
-    if (token == null || expiration == null) return false;
-
-    return DateTime.now().isBefore(expiration);
-  }
-
-  /// LOGOUT
-  static Future<void> logout() async {
+  @override
+  Future<void> logout() async {
     await _storage.delete(key: _tokenKey);
     await _storage.delete(key: _expirationKey);
+    _token = null;
+    _expiration = null;
+    _usuario = null;
+    await _storage.delete(key: _usuarioKey);
   }
 
-  // Confere se é admin
-  static Future<bool> isAdmin() async => await obterPerfil() == 1;
+  @override
+  Future<void> restaurarSessao() async {
+    _token = await _storage.read(key: _tokenKey);
+
+    final expirationString = await _storage.read(key: _expirationKey);
+
+    if (expirationString != null) {
+      _expiration = DateTime.tryParse(expirationString);
+    }
+
+    final usuarioJson = await _storage.read(key: _usuarioKey);
+
+    if (usuarioJson != null) {
+      _usuario = UsuarioResponse.fromJson(
+        jsonDecode(usuarioJson),
+      );
+    }
+  }
+
+  @override
+  String? get obterToken => _token;
+  
+  @override
+  bool get isAdmin => _usuario?.perfil == 1;
+
+  @override
+  UsuarioResponse? get usuario => _usuario;
+
 }
